de6485e1ff18733780bd4df7f917f53cb6a89d0e
Rijul Kapoor <rijulkapoor@gmail.com>
2024-09-26 13:10:36 +1000

    Removed format on save

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -14,14 +14,14 @@ return {
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- local disable_filetypes = { c = true, cpp = true }
-        local disable_filetypes = {}
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
+      -- format_on_save = function(bufnr)
+      --   -- local disable_filetypes = { c = true, cpp = true }
+      --   local disable_filetypes = {}
+      --   return {
+      --     timeout_ms = 500,
+      --     lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      --   }
+      -- end,
       formatters_by_ft = {
         lua = { 'stylua' },
         python = { 'black' },
