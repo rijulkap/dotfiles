f163d7ddb72467e24db459c704bec77b6c85e349
Rijul Kapoor <rijulkapoor@gmail.com>
2024-10-10 08:15:47 +1000

    Added other markdown plugin

---
+++ nvim/lua/plugins/markdown-stuff.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'iamcco/markdown-preview.nvim',
+    -- cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { 'markdown' },
+    config = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
+  {
+    'OXY2DEV/markview.nvim',
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+  },
+}
