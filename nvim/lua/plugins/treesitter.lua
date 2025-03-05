f26c7f839f1a3fff2ced8de35a70b9920840267f
Rijul Kapoor <rijulkapoor@gmail.com>
2025-03-05 12:40:26 +1000

    Treesiter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,11 +2,56 @@ return {
     { -- Highlight, edit, and navigate code
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "nvim-treesitter/nvim-treesitter-context"
+        },
         opts = {
             ensure_installed = { 'bash', 'c', 'cpp', 'c_sharp', 'python', 'rust', 'json', 'markdown', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
             auto_install = true,
             highlight = {
                 enable = true,
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ['af'] = '@function.outer',
+                        ['if'] = '@function.inner',
+                        ['ac'] = '@class.outer',
+                        ['ic'] = '@class.inner',
+                        ['a,'] = '@parameter.outer',
+                        ['i,'] = '@parameter.inner',
+                    },
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        [']f'] = '@function.outer',
+                        [']c'] = '@class.outer',
+                        ['],'] = '@parameter.inner',
+                    },
+                    goto_next_end = {
+                        [']F'] = '@function.outer',
+                        [']C'] = '@class.outer',
+                    },
+                    goto_previous_start = {
+                        ['[f'] = '@function.outer',
+                        ['[c'] = '@class.outer',
+                        ['[,'] = '@parameter.inner',
+                    },
+                    goto_previous_end = {
+                        ['[F'] = '@function.outer',
+                        ['[C'] = '@class.outer',
+                    },
+                },
+                swap = {
+                    enable = true,
+                    swap_next = { ['>,'] = '@parameter.inner' },
+                    swap_previous = { ['<,'] = '@parameter.inner' },
+                },
             },
             indent = { enable = true },
         },
