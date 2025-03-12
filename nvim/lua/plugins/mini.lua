994cf9c57b7f96dc76907e91ae13173b4e1ee608
Rijul Kapoor <rijulkapoor@gmail.com>
2025-03-12 16:44:53 +1000

    Added mini ai

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -2,6 +2,7 @@ return {
     { -- Collection of various small independent plugins/modules
         'echasnovski/mini.nvim',
         config = function()
+            require('mini.ai').setup()
             require('mini.move').setup()
             require('mini.surround').setup()
         end,
