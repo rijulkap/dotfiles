06719bcff2ce761bc70bd32d5ae67f6d9c9a52ff
Rijul Kapoor <rijulkapoor@gmail.com>
2025-03-21 09:27:01 +1000

    fotsize wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -20,6 +20,7 @@ config.font = wezterm.font_with_fallback({
     { family = "JetBrainsMono Nerd Font", scale = 1.0, weight = "Medium" },
 })
 
+config.font_size = 11
 config.underline_thickness = 2
 
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
