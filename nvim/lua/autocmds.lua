20c1fcb319b008e1cb0a7d283188c6120a576533
Rijul Kapoor <rijulkapoor@gmail.com>
2025-03-21 09:53:34 +1000

    stop showing spelling errors in floating windows
    spelling errors in float fix

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -9,9 +9,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'typst', 'markdown' },
     callback = function()
+        if vim.bo.buftype ~= 'nofile' then
+            vim.o.spell = true
+        end
         vim.o.wrap = true
         vim.o.spelllang = 'en_us'
-        vim.o.spell = true
         vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
         vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
     end,
