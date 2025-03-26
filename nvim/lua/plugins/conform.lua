991881ec8ba1a8218b08144d24705798b0e5f0b0
Rijul Kapoor <rijulkapoor@gmail.com>
2025-03-26 13:37:40 +1000

    Format on save toggle
    Made it a bit cleaner

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -12,30 +12,55 @@ return {
                 desc = "[F]ormat buffer",
             },
         },
-        opts = {
-            notify_on_error = false,
-            format_on_save = function(bufnr)
-                -- local disable_filetypes = { c = true, cpp = true }
-                local disable_filetypes = {}
-                local lsp_format_opt
-                if disable_filetypes[vim.bo[bufnr].filetype] then
-                    lsp_format_opt = "never"
-                else
-                    lsp_format_opt = "fallback"
-                end
-                return {
-                    timeout_ms = 500,
-                    lsp_format = lsp_format_opt,
-                }
-            end,
-            formatters_by_ft = {
-                lua = { "stylua" },
-            },
-            formatters = {
-                stylua = {
-                    prepend_args = { "--indent-type", "Spaces", "--indent-width", "4" },
+        opts = function()
+            return {
+                notify_on_error = false,
+                format_on_save = function(bufnr)
+                    if vim.g.autoformat then
+                        -- local disable_filetypes = { c = true, cpp = true }
+                        local disable_filetypes = {}
+                        local lsp_format_opt
+                        if disable_filetypes[vim.bo[bufnr].filetype] then
+                            lsp_format_opt = "never"
+                        else
+                            lsp_format_opt = "fallback"
+                        end
+                        return {
+                            timeout_ms = 500,
+                            lsp_format = lsp_format_opt,
+                        }
+                    else
+                        return
+                    end
+                end,
+                formatters_by_ft = {
+                    lua = { "stylua" },
                 },
-            },
-        },
+                formatters = {
+                    stylua = {
+                        prepend_args = { "--indent-type", "Spaces", "--indent-width", "4" },
+                    },
+                },
+            }
+        end,
+    },
+    {
+        "folke/snacks.nvim",
+        opts = function()
+            vim.g.autoformat = true
+
+            Snacks.toggle
+                .new({
+                    id = "Format on Save",
+                    name = "Format on Save",
+                    get = function()
+                        return vim.g.autoformat
+                    end,
+                    set = function(_)
+                        vim.g.autoformat = not vim.g.autoformat
+                    end,
+                })
+                :map("<leader>uf")
+        end,
     },
 }
