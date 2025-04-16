edf1fe3a1f7d4b7298b8f9f017bfc987898c0e52
Rijul Kapoor <rijulkapoor@gmail.com>
2025-04-16 13:08:55 +1000

    some blink signature settings
    Upgraded blink version

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -5,9 +5,7 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         -- optional: provides snippets for the snippet source
         dependencies = "rafamadriz/friendly-snippets",
-
-        -- use a release tag to download pre-built binaries
-        version = "v0.*",
+        version = "1.*",
         -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
         -- build = 'cargo build --release',
         -- On musl libc based systems you need to add this flag
@@ -91,8 +89,7 @@ return {
             -- experimental auto-brackets support
             -- accept = { auto_brackets = { enabled = true } }
 
-            -- experimental signature help support
-            -- trigger = { signature_help = { enabled = true } }
+            signature = { enabled = true },
         },
     },
 }
