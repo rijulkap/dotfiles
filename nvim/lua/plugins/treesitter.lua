a347aa83465eb01998bd3f74b195f7fb0f229d36
Rijul Kapoor <rijulkapoor@gmail.com>
2025-05-07 09:38:52 +1000

    feat(nvim): added go to context

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,37 +4,20 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         build = ":TSUpdate",
         dependencies = {
-            "nvim-treesitter/nvim-treesitter-context",
-        },
-        opts = {
-            ensure_installed = {
-                "bash",
-                "c",
-                "cpp",
-                "c_sharp",
-                "python",
-                "rust",
-                "json",
-                "markdown",
-                "html",
-                "lua",
-                "luadoc",
-                "markdown",
-                "vim",
-                "vimdoc",
-                "javascript",
-                "typescript",
-                "tsx",
+            {
+                "nvim-treesitter/nvim-treesitter-context",
+                opts = function()
+                    vim.keymap.set("n", "[c", function()
+                        require("treesitter-context").go_to_context(vim.v.count1)
+                    end, { silent = true })
+                    return {
+                        -- max_lines = 10,
+                    }
+                end,
             },
-            -- auto_install = true,
-            highlight = {
-                enable = true,
-            },
-            indent = { enable = true },
         },
-        config = function(_, opts)
+        opts = function()
             ---@diagnostic disable-next-line: missing-fields
-            require("nvim-treesitter.configs").setup(opts)
             vim.opt.foldmethod = "expr"
             --
             -- -- :h vim.treesitter.foldexpr()
@@ -43,7 +26,32 @@ return {
             -- -- ref: https://github.com/neovim/neovim/pull/20750
             vim.opt.foldtext = ""
             vim.opt.fillchars:append("fold: ")
-            --
+            return {
+                ensure_installed = {
+                    "bash",
+                    "c",
+                    "cpp",
+                    "c_sharp",
+                    "python",
+                    "rust",
+                    "json",
+                    "markdown",
+                    "html",
+                    "lua",
+                    "luadoc",
+                    "markdown",
+                    "vim",
+                    "vimdoc",
+                    "javascript",
+                    "typescript",
+                    "tsx",
+                },
+                -- auto_install = true,
+                highlight = {
+                    enable = true,
+                },
+                indent = { enable = true },
+            }
         end,
     },
 }
