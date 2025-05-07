6b764c6c3d48a2028005ac9cb36a65c37fbafdf1
Rijul Kapoor <rijulkapoor@gmail.com>
2025-05-07 14:20:55 +1000

    fix(nvim): fixed supertab implementation

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -34,14 +34,13 @@ return {
                 ghost_text = { enabled = true },
             },
             keymap = {
-                ["<CR>"] = { "accept", "fallback" },
                 ["<C-e>"] = { "hide", "fallback" },
 
                 ["<C-n>"] = { "select_next", "show" },
                 ["<C-p>"] = { "select_prev" },
 
-                ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
-                ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+                ["<Tab>"] = { "accept", "snippet_forward", "fallback" },
+                ["<S-Tab>"] = { "snippet_backward", "fallback" },
 
                 ["<C-b>"] = { "scroll_documentation_up", "fallback" },
                 ["<C-f>"] = { "scroll_documentation_down", "fallback" },
