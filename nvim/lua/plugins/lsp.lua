1e7127a63e57f8154be0c154ff61861ac3c0b53c
Rijul Kapoor <rijulkapoor@gmail.com>
2025-05-13 22:21:17 +1000

    fix(nvim): Added luajit lua_ls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -51,8 +51,15 @@ vim.g.lsp_servers = {
     lua_ls = {
         settings = {
             Lua = {
-                completion = {
-                    callSnippet = "Replace",
+                completion = { callSnippet = "Replace" },
+                -- Using stylua for formatting.
+                format = { enable = false },
+                hint = {
+                    enable = true,
+                    arrayIndex = "Disable",
+                },
+                runtime = {
+                    version = "LuaJIT",
                 },
             },
         },
