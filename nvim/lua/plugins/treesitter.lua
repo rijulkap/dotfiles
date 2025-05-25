fe8c9cf4953f3fb7ae7e700a9678f9ee8447f850
Rijul Kapoor <rijulkapoor@gmail.com>
2025-05-25 14:10:38 +1000

    fix(nvim): changed treesitter branch

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     { -- Highlight, edit, and navigate code
         "nvim-treesitter/nvim-treesitter",
-        event = { "BufReadPre", "BufNewFile" },
+        branch = "main",
         build = ":TSUpdate",
         dependencies = {
             {
