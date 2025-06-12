aedc1c123fb7db97baf581a33a76e7e3e50ae1aa
Rijul Kapoor <rijulkapoor@gmail.com>
2025-06-12 14:27:38 +1000

    fix(nvim): updated dap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,252 +1,230 @@
+-- Set up icons.
+local icons = {
+    Stopped = { "", "DiagnosticWarn", "DapStoppedLine" },
+    Breakpoint = "",
+    BreakpointCondition = "",
+    BreakpointRejected = { "", "DiagnosticError" },
+}
+for name, sign in pairs(icons) do
+    sign = type(sign) == "table" and sign or { sign }
+    vim.fn.sign_define("Dap" .. name, {
+        -- stylua: ignore
+        text = sign[1] --[[@as string]] .. ' ',
+        texthl = sign[2] or "DiagnosticInfo",
+        linehl = sign[3],
+        numhl = sign[3],
+    })
+end
+
 return {
     {
-        -- NOTE: Yes, you can install new plugins here!
-        'mfussenegger/nvim-dap',
+        "mfussenegger/nvim-dap",
         dependencies = {
-            -- Creates a beautiful debugger UI
             {
-                'rcarriga/nvim-dap-ui',
-                keys = {
-                    {
-                        '<leader>du',
-                        function()
-                            require('dapui').toggle {}
-                        end,
-                        desc = 'Dap UI',
+                "igorlfs/nvim-dap-view",
+                opts = {
+                    winbar = {
+                        sections = { "scopes", "breakpoints", "threads", "exceptions", "repl", "console" },
+                        default_section = "scopes",
                     },
-                    {
-                        '<leader>de',
-                        function()
-                            require('dapui').eval()
-                        end,
-                        desc = 'Eval',
-                        mode = { 'n', 'v' },
-                    },
+                    windows = { height = 18 },
+                    switchbuf = "usetab,uselast",
                 },
             },
-            -- Required dependency for nvim-dap-ui
-            'nvim-neotest/nvim-nio',
-
-            -- Installs the debug adapters for you
-            { 'williamboman/mason.nvim', opts = {} },
-            'jay-babu/mason-nvim-dap.nvim',
+            {
+                "theHamsta/nvim-dap-virtual-text",
+                opts = { virt_text_pos = "eol" },
+            },
+            { "williamboman/mason.nvim", opts = {} },
+            "jay-babu/mason-nvim-dap.nvim",
         },
         keys = {
             {
-                '<leader>dB',
+                "<leader>dB",
                 function()
-                    require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+                    require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
                 end,
-                desc = 'Breakpoint Condition',
+                desc = "Breakpoint Condition",
             },
             {
-                '<leader>db',
+                "<leader>db",
                 function()
-                    require('dap').toggle_breakpoint()
+                    require("dap").toggle_breakpoint()
                 end,
-                desc = 'Toggle Breakpoint',
+                desc = "Toggle Breakpoint",
             },
             {
-                '<leader>dc',
+                "<leader>dc",
                 function()
-                    require('dap').continue()
+                    require("dap").continue()
                 end,
-                desc = 'Continue',
+                desc = "Continue",
             },
             {
-                '<leader>dC',
+                "<leader>dC",
                 function()
-                    require('dap').run_to_cursor()
+                    require("dap").run_to_cursor()
                 end,
-                desc = 'Run to Cursor',
+                desc = "Run to Cursor",
             },
             {
-                '<leader>dg',
+                "<leader>dg",
                 function()
-                    require('dap').goto_()
+                    require("dap").goto_()
                 end,
-                desc = 'Go to line (no execute)',
+                desc = "Go to line (no execute)",
             },
             {
-                '<leader>di',
+                "<leader>di",
                 function()
-                    require('dap').step_into()
+                    require("dap").step_into()
                 end,
-                desc = 'Step Into',
+                desc = "Step Into",
             },
             {
-                '<leader>dj',
+                "<leader>dj",
                 function()
-                    require('dap').down()
+                    require("dap").down()
                 end,
-                desc = 'Down',
+                desc = "Down",
             },
             {
-                '<leader>dk',
+                "<leader>dk",
                 function()
-                    require('dap').up()
+                    require("dap").up()
                 end,
-                desc = 'Up',
+                desc = "Up",
             },
             {
-                '<leader>dl',
+                "<leader>dl",
                 function()
-                    require('dap').run_last()
+                    require("dap").run_last()
                 end,
-                desc = 'Run Last',
+                desc = "Run Last",
             },
             {
-                '<leader>do',
+                "<leader>do",
                 function()
-                    require('dap').step_out()
+                    require("dap").step_out()
                 end,
-                desc = 'Step Out',
+                desc = "Step Out",
             },
             {
-                '<leader>dO',
+                "<leader>dO",
                 function()
-                    require('dap').step_over()
+                    require("dap").step_over()
                 end,
-                desc = 'Step Over',
+                desc = "Step Over",
             },
             {
-                '<leader>dp',
+                "<leader>dp",
                 function()
-                    require('dap').pause()
+                    require("dap").pause()
                 end,
-                desc = 'Pause',
+                desc = "Pause",
             },
             {
-                '<leader>dr',
+                "<leader>dr",
                 function()
-                    require('dap').repl.toggle()
+                    require("dap").repl.toggle()
                 end,
-                desc = 'Toggle REPL',
+                desc = "Toggle REPL",
             },
             {
-                '<leader>ds',
+                "<leader>ds",
                 function()
-                    require('dap').session()
+                    require("dap").session()
                 end,
-                desc = 'Session',
+                desc = "Session",
             },
             {
-                '<leader>dt',
+                "<leader>dt",
                 function()
-                    require('dap').terminate()
+                    require("dap").terminate()
                 end,
-                desc = 'Terminate',
+                desc = "Terminate",
             },
             {
-                '<leader>dw',
+                "<leader>du",
                 function()
-                    require('dap.ui.widgets').hover()
+                    require("dap-view").close(true)
                 end,
-                desc = 'Widgets',
+                desc = "Close UI",
             },
         },
         config = function()
-            local dap = require 'dap'
-            local dapui = require 'dapui'
+            local dap = require("dap")
+            local dv = require("dap-view")
 
-            vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = true, link = 'Visual' })
+            dap.listeners.before.attach["dap-view-config"] = function()
+                dv.open()
+            end
+            dap.listeners.before.launch["dap-view-config"] = function()
+                dv.open()
+            end
+            dap.listeners.before.event_terminated["dap-view-config"] = function()
+                dv.close()
+            end
+            dap.listeners.before.event_exited["dap-view-config"] = function()
+                dv.close()
+            end
 
-            require('mason-nvim-dap').setup {
-                -- Makes a best effort to setup the various debuggers with
-                -- reasonable debug configurations
-                automatic_installation = true,
+            local InstallLocation = require("mason-core.installer.InstallLocation")
 
-                -- You can provide additional configuration to the handlers,
-                -- see mason-nvim-dap README for more information
-                handlers = {},
-
-                -- You'll need to check that you have the required things installed
-                -- online, please don't ask me how to install them :)
-                ensure_installed = {
-                    -- Update this to ensure that you have the debuggers for the langs you want
-                    'codelldb',
-                    'netcoredbg',
-                },
-            }
-
-            local netcoredbg = require('mason-registry').get_package 'netcoredbg'
-            local netcoredbg_path = netcoredbg:get_install_path() .. '/netcoredbg/netcoredbg.exe'
+            -- Adapter: CoreCLR (.NET)
+            local netcoredbg_root = InstallLocation.global():package("netcoredbg")
+            local netcoredbg_exe = vim.fs.joinpath(netcoredbg_root, "netcoredbg", "netcoredbg.exe")
 
             dap.adapters.coreclr = {
-                type = 'executable',
-                command = netcoredbg_path,
-                args = { '--interpreter=vscode' },
+                type = "executable",
+                command = netcoredbg_exe,
+                args = { "--interpreter=vscode" },
+                options = { detached = false },
             }
 
             dap.configurations.cs = {
                 {
-                    type = 'coreclr',
-                    name = 'launch - netcoredbg',
-                    request = 'launch',
+                    type = "coreclr",
+                    name = "launch - netcoredbg",
+                    request = "launch",
                     program = function()
-                        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+                        return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
                     end,
+                    stopOnEntry = true,
                 },
             }
 
-            local codelldb = require('mason-registry').get_package 'codelldb'
-            local codelldb_extension_path = codelldb:get_install_path() .. '/extension/'
-            local codelldb_path = codelldb_extension_path .. 'adapter/codelldb'
+            -- Adapter: CodeLLDB (Rust, C, C++)
+            local codelldb_root = InstallLocation.global():package("codelldb")
+            local codelldb_exe = vim.fs.joinpath(codelldb_root, "extension", "adapter", "codelldb")
 
             dap.adapters.codelldb = {
-                type = 'server',
-                port = '${port}',
+                type = "server",
+                port = "${port}",
                 executable = {
-                    -- CHANGE THIS to your path!
-                    command = codelldb_path,
-                    args = { '--port', '${port}' },
+                    command = codelldb_exe,
+                    args = { "--port", "${port}" },
 
                     -- On windows you may have to uncomment this:
-                    -- detached = false,
                 },
+                -- detached = false,
             }
 
             dap.configurations.rust = {
                 {
-                    name = 'Launch file',
-                    type = 'codelldb',
-                    request = 'launch',
+                    name = "Launch file",
+                    type = "codelldb",
+                    request = "launch",
                     program = function()
-                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
                     end,
-                    cwd = '${workspaceFolder}',
+                    cwd = "${workspaceFolder}",
                     stopOnEntry = false,
                 },
             }
 
             dap.configurations.c = dap.configurations.rust
             dap.configurations.cpp = dap.configurations.rust
-
-            -- Dap UI setup
-            -- For more information, see |:help nvim-dap-ui|
-            dapui.setup {
-                -- Set icons to characters that are more likely to work in every terminal.
-                --    Feel free to remove or use ones that you like more! :)
-                --    Don't feel like these are good choices.
-                icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-                controls = {
-                    icons = {
-                        pause = '⏸',
-                        play = '▶',
-                        step_into = '⏎',
-                        step_over = '⏭',
-                        step_out = '⏮',
-                        step_back = 'b',
-                        run_last = '▶▶',
-                        terminate = '⏹',
-                        disconnect = '⏏',
-                    },
-                },
-            }
-
-            dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-            dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-            dap.listeners.before.event_exited['dapui_config'] = dapui.close
         end,
     },
 }
