f7519eda55e74632f334dae7f320ff29972eeab2
Rijul Kapoor <rijulkapoor@gmail.com>
2025-10-13 12:23:40 +1000

    fix(nvim): fixed nvim windows opts

--- nvim/lua/win_bootstrap.lua
+++ nvim/lua/win_bootstrap.lua
@@ -1,13 +1,13 @@
-if vim.fn.exists 'g:os' == 0 then
-    local is_windows = vim.fn.has 'win64' == 1 or vim.fn.has 'win32' == 1 or vim.fn.has 'win16' == 1
+if vim.fn.exists("g:os") == 0 then
+    local is_windows = vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 or vim.fn.has("win16") == 1
     if is_windows then
-        -- vim.opt.shell = vim.fn.executable 'pwsh' == 1 and 'pwsh' or 'powershell'
-        vim.opt.shell = 'powershell'
+        vim.opt.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
+        -- vim.opt.shell = 'powershell'
         vim.opt.shellcmdflag =
-        '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-        vim.opt.shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
-        vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-        vim.opt.shellquote = ''
-        vim.opt.shellxquote = ''
+            "-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
+        vim.o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+        vim.o.shellpipe = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+        vim.opt.shellquote = ""
+        vim.opt.shellxquote = ""
     end
 end
