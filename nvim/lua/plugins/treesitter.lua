6b81338fcba2fa590bf369551876a0fe29670e07
Rijul Kapoor <rijulkapoor@gmail.com>
2026-03-25 16:00:24 +1000

    feat: added build autocommand to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -47,6 +47,17 @@ setup_ts = function()
         "typescript",
         "tsx",
     })
+    vim.api.nvim_create_autocmd("PackChanged", {
+        callback = function(ev)
+            local name, kind = ev.data.spec.name, ev.data.kind
+            if name == "nvim-treesitter" and kind == "update" then
+                if not ev.data.active then
+                    vim.cmd.packadd("nvim-treesitter")
+                end
+                vim.cmd("TSUpdate")
+            end
+        end,
+    })
 
     vim.api.nvim_create_autocmd("FileType", {
         pattern = { "<filetype>" },
