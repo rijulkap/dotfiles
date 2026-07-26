802d8ac7ec6a4f3e944870e37ee37ec129a85fef
Rijul Kapoor <115141708+rijulkap@users.noreply.github.com>
2026-07-26 21:22:23 +1000

    feat: added support for dotstrap 0.1.3

--- download_dotstrap.ps1
+++ download_dotstrap.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
-$Version = "v0.1.2"
+$Version = "v0.1.3"
 $Repository = "rijulkap/dotstrap"
 
 if ($PSVersionTable.PSEdition -eq "Core" -and -not $IsWindows) {
