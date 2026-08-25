8928cd8e30a0e24eef47bc0aaa23c120a4551ef5
Rijul Kapoor <rijulkapoor@gmail.com>
2026-08-25 11:21:35 +1000

    fix(nvim): slightly improve startup time

--- nvim/lua/win_bootstrap.lua
+++ nvim/lua/win_bootstrap.lua
@@ -4,7 +4,11 @@ if vim.fn.exists("g:os") == 0 then
         vim.opt.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
         -- vim.opt.shell = 'powershell'
         vim.opt.shellcmdflag =
-            "-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
+          "-NoLogo -NoProfile -NonInteractive -ExecutionPolicy RemoteSigned -Command " ..
+          "[Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();" ..
+          "$PSDefaultParameterValues['Out-File:Encoding']='utf8';" ..
+          "$PSStyle.OutputRendering='plaintext';" ..
+          "Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
         vim.o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
         vim.o.shellpipe = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
         vim.opt.shellquote = ""
